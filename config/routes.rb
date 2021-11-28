02a4a9d08b40e039cadb18d07167e7477abfeb58
Connor <conmmcguire@gmail.com>
2021-11-28 16:47:23 -0600

    innit and authorization models/controllers

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/users" => "users#create"
+
+  post "/sessions" => "sessions#create"
+
+end
